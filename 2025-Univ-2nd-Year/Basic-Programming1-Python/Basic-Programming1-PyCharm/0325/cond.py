aa5edca83c7183f00fc6291caf356a8aa613893b
xxanecdote@naver.com <xxanecdote@naver.com>
2025-03-25 12:39:47 +0900

    Update

---
+++ 2025-Univ-2nd-Year/Basic-Programming1-Python/Basic-Programming1-PyCharm/0325/cond.py
@@ -0,0 +1,19 @@
+print("# Compound boolean expressions #")
+# 변수 값 설정
+x = 10
+y = 8
+z = 5
+
+# 최솟값 판단
+if x < y and x < z:
+    print('x is least')
+elif y < z:
+    print('y is least')
+else:
+    print('z is least')
+
+print("# opeartor & operand #")
+# opeartor *
+# operand 3,4
+a = "hi"
+print(3*a)
